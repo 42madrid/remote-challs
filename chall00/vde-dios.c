97ab5dd15adbc0ec783dc225ab1ecd9b4ec34e32
Victor <victor.dedios@outlook.com>
2020-03-30 21:51:01 +0200

    add hex function to make code more human readable

--- chall00/vde-dios.c
+++ chall00/vde-dios.c
@@ -1,5 +1,10 @@
 #include <stdlib.h>
 
+char	hex(char n)
+{
+	return (n > 9 ? (n + 'a' -10) : (n + '0'));
+}
+
 char	*hv_rgb2hex(int r, int g, int b)
 {
 	char	*rgb;
@@ -9,12 +14,12 @@ char	*hv_rgb2hex(int r, int g, int b)
 	if (!(rgb = malloc((8) * sizeof(char))))
 		return (0);
 	rgb[0] = '#';
-	rgb[1] = r & 0xF0 ? (((r & 0xF0) >> 4) > 9 ? ((r & 0xF0) >> 4) + 'a' - 10 : ((r & 0xF0) >> 4) + '0') : 0 + '0';
-	rgb[2] = r & 0x0F ? ((r & 0x0F) > 9 ? (r & 0x0F) + 'a' - 10 : (r & 0x0F) + '0') : 0 + '0';
-	rgb[3] = g & 0xF0 ? (((g & 0xF0) >> 4) > 9 ? ((g & 0xF0) >> 4) + 'a' - 10 : ((g & 0xF0) >> 4) + '0') : 0 + '0';
-	rgb[4] = g & 0x0F ? ((g & 0x0F) > 9 ? (g & 0x0F) + 'a' - 10 : (g & 0x0F) + '0') : 0 + '0';
-	rgb[5] = b & 0xF0 ? (((b & 0xF0) >> 4) > 9 ? ((b & 0xF0) >> 4) + 'a' - 10 : ((b & 0xF0) >> 4) + '0') : 0 + '0';
-	rgb[6] = b & 0x0F ? ((b & 0x0F) > 9 ? (b & 0x0F) + 'a' - 10 : (b & 0x0F) + '0') : 0 + '0';
+	rgb[1] = r & 0xF0 ? hex((r & 0xF0) >> 4) : 0 + '0';
+	rgb[2] = r & 0x0F ? hex(r & 0x0F) : 0 + '0';
+	rgb[3] = g & 0xF0 ? hex((g & 0xF0) >> 4) : 0 + '0';
+	rgb[4] = g & 0x0F ? hex(g & 0x0F) : 0 + '0';
+	rgb[5] = b & 0xF0 ? hex((b & 0xF0) >> 4) : 0 + '0';
+	rgb[6] = b & 0x0F ? hex(b & 0x0F) : 0 + '0';
 	rgb[7] = '\0';
 	return (rgb);
 }
