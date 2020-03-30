ac0f0d990093034f20440e27ff7e1eada7c1d89b
David Rodríguez <9976038+d-r-e@users.noreply.github.com>
2020-03-30 15:41:22 +0200

    Fixed '#'

--- darodrig.c
+++ darodrig.c
@@ -16,17 +16,18 @@
 
 char	*hv_rgb2hex(int r, int g, int b)
 {
-	char result[7];
+	char result[8];
 	char caps[] = "0123456789abcdef";
 	if (r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255)
-		return (strdup("000000"));
+		return (strdup("#000000"));
 	
-	result[0] = caps[r / 16];
-	result[1] = caps[r % 16];
-	result[2] = caps[g / 16];
-	result[3] = caps[g % 16];
-	result[4] = caps[b / 16];
-	result[5] = caps[b % 16];
-	result[6] = 0;
+	result[0] = '#';
+	result[1] = caps[r / 16];
+	result[2] = caps[r % 16];
+	result[3] = caps[g / 16];
+	result[4] = caps[g % 16];
+	result[5] = caps[b / 16];
+	result[6] = caps[b % 16];
+	result[7] = 0;
 	return (strdup(result));
 }
