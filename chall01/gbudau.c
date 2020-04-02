f5a5ec9de03b04678dbaacca61bd2c82ec05eca3
gbudau <gbudau@student.42madrid.com>
2020-04-02 22:52:25 +0200

    renamed variables, starting by iterating s1 now

--- chall01/gbudau.c
+++ chall01/gbudau.c
@@ -1,41 +1,41 @@
-static int check_necklace(char *s1, char *s2)
+static int check_necklace(char *first, char *second)
 {
 	int	i;
        	int	j;
-	int	k;
-	int	l;
+	int	start;
+	int	checked;
 
-	if (!s1 || !s2)
+	if (!first || !second)
 		return (0);
 	i = 0;
 	j = 0;
-	while (s2[i])
+	while (first[i])
 	{
-		if (s2[i] == s1[j])
+		if (first[i] == second[j])
 		{
 			j = 0;
-			while (s1[j] && s2[i + j] && s2[i + j] == s1[j])
+			while (first[i + j] && second[j] && first[i + j] == second[j])
 				j++;
-			if (!s2[i + j])
+			if (!first[i + j])
 				break;
 		}
 		i++;
 	}
-	k = 0;
-	l = j;
-	while (s1[j] && s2[k] && k < i)
+	start = 0;
+	checked = j;
+	while (first[start] && second[j] && start < i)
 	{
-		if (s2[k] != s1[j])
+		if (first[start] != second[j])
 			break;
-		k++;
+		start++;
 		j++;
 	}
-	return (i + l == j && !s1[i + l] && !s2[j]);
+	return (i + checked == j && !second[i + checked] && !first[j]);
 }
 
-int hv_necklace(char *s1, char *s2)
+int hv_necklace(char *first, char *second)
 {
-	if (!check_necklace(s1, s2))
-		return (check_necklace(s2, s1));
+	if (!check_necklace(first, second))
+		return (check_necklace(second, first));
 	return (1);
 }
