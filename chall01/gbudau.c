6d151c0914f220d8df35617921dc00e2167240cd
gbudau <gbudau@student.42madrid.com>
2020-04-02 20:54:17 +0200

    chall01 - gbudau

---
+++ chall01/gbudau.c
@@ -0,0 +1,34 @@
+int hv_necklace(char *s1, char *s2)
+{
+	int	i;
+       	int	j;
+	int	k;
+	int	l;
+
+	if (!s1 || !s2)
+		return (0);
+	i = 0;
+	j = 0;
+	while (s2[i])
+	{
+		if (s2[i] == s1[j])
+		{
+			j = 0;
+			while (s1[j] && s2[i + j] && s2[i + j] == s1[j])
+				j++;
+			if (!s2[i + j])
+				break;
+		}
+		i++;
+	}
+	k = 0;
+	l = j;
+	while (s1[j] && s2[k] && k < i)
+	{
+		if (s2[k] != s1[j])
+			break;
+		k++;
+		j++;
+	}
+	return (i + l == j && !s1[i + l] && !s2[j]);
+}
