8380741e2196cb4440703ab0a47ad4c7af59c56c
gbudau <gbudau@student.42madrid.com>
2020-04-03 03:15:33 +0200

    chal001 - gbudau, code cleanup

--- chall01/gbudau.c
+++ chall01/gbudau.c
@@ -1,41 +1,29 @@
-static int check_necklace(char *first, char *second)
+int hv_necklace(char *first, char *second)
 {
 	int	i;
        	int	j;
-	int	start;
 	int	checked;
 
-	if (!first || !second)
-		return (0);
 	i = 0;
 	j = 0;
 	while (first[i])
 	{
 		if (first[i] == second[j])
 		{
-			j = 0;
 			while (first[i + j] && second[j] && first[i + j] == second[j])
 				j++;
 			if (!first[i + j])
 				break;
+			j = 0;
 		}
 		i++;
 	}
-	start = 0;
+	i = 0;
 	checked = j;
-	while (first[start] && second[j] && start < i)
+	while (first[i] && second[j] && first[i] == second[j])
 	{
-		if (first[start] != second[j])
-			break;
-		start++;
+		i++;
 		j++;
 	}
-	return (i + checked == j && !second[i + checked] && !first[j]);
-}
-
-int hv_necklace(char *first, char *second)
-{
-	if (!check_necklace(first, second))
-		return (check_necklace(second, first));
-	return (1);
+	return (!first[i + checked] && !second[j]);
 }
