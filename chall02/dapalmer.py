5d7cfe2bffcf8522828904797d9dd0de88ce2af6
David Palmero <dapalm44@gmail.com>
2020-04-06 12:47:13 +0200

    Python morse converter

---
+++ chall02/dapalmer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import sys
+
+def main():
+    morseCode = {
+        'A': '.-',
+        'B': '-...',
+        'C': '-.-.',
+        'D': '-..',
+        'E': '.',
+        'F': '..-.',   
+        'G': '--.',
+        'H': '....',
+        'I': '..',   
+        'J': '.---',
+        'K': '-.-',
+        'L': '.-..',   
+        'M': '--',
+        'N': '-.',
+        'O': '---',   
+        'P': '.--.',
+        'Q': '--.-',
+        'R': '.-.',
+        'S': '...',
+        'T': '-',
+        'U': '..-',
+        'V': '...-',
+        'W': '.--',
+        'X': '-..-',
+        'Y': '-.--',
+        'Z': '--..',       
+    }
+    origString = sys.argv[1].upper()
+    morseString = ''
+
+    for char in origString:
+        if char >= 'A' and char <= 'Z':
+            morseString += morseCode[char]
+
+    print morseString
+
+if __name__ == "__main__":
+    main()
+
