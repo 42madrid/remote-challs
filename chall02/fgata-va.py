6e9678cd806f6b8e8e372d7f9d3d8a1e9b97256d
Francisco Jesús Gata <fjgatv8998@gmail.com>
2020-04-06 12:58:53 +0200

    fgata-va's solution

---
+++ chall02/fgata-va.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+import sys
+
+def errorCode():
+    print("usage: ./xlogin.py <a-zA-Z string>")
+    quit()
+
+if((len(sys.argv) > 2) or (len(sys.argv) == 1)):
+    errorCode()
+if(len(sys.argv[1]) == 0):
+    errorCode()
+
+arguments = sys.argv[1].split(' ')
+morse = ""
+
+for argu in arguments:
+    if((len(argu) == 0) or not(argu.isalpha())):
+        errorCode()
+    for c in argu.lower():
+        if (c == 'a'):
+            morse += '.-'
+        elif (c == 'b'):
+            morse += '-...'
+        elif (c == 'b'):
+            morse += '-...'
+        elif (c == 'c'):
+            morse += '-.-.'
+        elif (c == 'd'):
+            morse += '-..'
+        elif (c == 'e'):
+            morse += '.'
+        elif (c == 'f'):
+            morse += '..-.'
+        elif (c == 'g'):
+            morse += '--.'
+        elif (c == 'h'):
+            morse += '....'
+        elif (c == 'i'):
+            morse += '..'
+        elif (c == 'j'):
+            morse += '.---'
+        elif (c == 'k'):
+            morse += '.-.'
+        elif (c == 'l'):
+            morse += '.-..'
+        elif (c == 'm'):
+            morse += '--'
+        elif (c == 'n'):
+            morse += '-.'
+        elif (c == 'o'):
+            morse += '---'
+        elif (c == 'p'):
+            morse += '.--.'
+        elif (c == 'q'):
+            morse += '--.-'
+        elif (c == 'r'):
+            morse += '.-.'
+        elif (c == 's'):
+            morse += '...'
+        elif (c == 't'):
+            morse += '-'
+        elif (c == 'u'):
+            morse += '..-'
+        elif (c == 'v'):
+            morse += '...-'
+        elif (c == 'w'):
+            morse += '.--'
+        elif (c == 'x'):
+            morse += '-..-'
+        elif (c == 'y'):
+            morse += '-.--'
+        elif (c == 'z'):
+            morse += '--..'
+    morse += ' '
+print(morse)
