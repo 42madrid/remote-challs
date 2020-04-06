3d71caf835110fc463382a0930d6e129de6f914f
Mario Pernia <57371690+mmiguelx@users.noreply.github.com>
2020-04-06 15:40:42 +0200

    mpernia-

---
+++ chall02/mpernia-.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import sys
+
+morse = {
+	'a':'.-',
+	'b':'-...',
+	'c':'-.-.',
+	'd':'-..',
+	'e':'.',
+	'f':'..-.',
+	'g':'--.',
+	'h':'....',
+	'i':'..',
+	'j':'.---',
+	'k':'-.-',
+	'l':'.-..',
+	'm':'--',
+	'n':'-.',
+	'o':'---',
+	'p':'.--.',
+	'q':'--.-',
+	'r':'.-.',
+	's':'...',
+	't':'-',
+	'u':'..-',
+	'v':'...-',
+	'w':'.--',
+	'x':'-..-',
+	'y':'-.--',
+	'z':'--..'
+}
+
+if len(sys.argv) == 2 and all(x.isalpha() or x.isspace() for x in sys.argv[1]) and len(sys.argv[1]) > 0:
+	string = str.lower(sys.argv[1])
+	for char in string:
+		if char != ' ':
+			print(morse[char], end='')
+		else:
+			print(' ', end='')
+	print('')
+else:
+	print("usage: ./xlogin.py <a--Z string>")
