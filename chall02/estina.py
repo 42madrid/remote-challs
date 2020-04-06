ead6a9be09dc816b18b0a822d9767002d2eff973
Eugeniu Stina <32365873+xplore93@users.noreply.github.com>
2020-04-06 20:32:11 +0200

    Removing all the comments.

--- chall02/estina.py
+++ chall02/estina.py
@@ -11,10 +11,8 @@
 #                                                                              #
 # **************************************************************************** #
 
-# For arguments
 import sys
 
-# Declare our dictionaire for the MORSE
 CHAR_TO_MORSE = {
 	" ": " ",
 	"a": ".-",
@@ -45,17 +43,11 @@ CHAR_TO_MORSE = {
 	"z": "--.."
 }
 
-# Check the number of arguments and if the second argument is not empty
 if len(sys.argv) != 2 or not sys.argv[1]:
 	exit("usage " + sys.argv[0] + " <a-zA-Z string>")
-# Declare variable for our new string
 string = ""
-# Passing through all of the characters
 for c in sys.argv[1]:
-	# Check if the character(lowered) is in our Dictionarie
 	if c.lower() not in CHAR_TO_MORSE:
 		exit("usage " + sys.argv[0] + " <a-zA-Z string>")
-	# Add the code of the character to our new string
 	string += CHAR_TO_MORSE.get(c)
-# Write the new string in MORSE to the terminal
 print(string)
