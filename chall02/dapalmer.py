165a33671414f5c78c1eac9d21dd0d14c0a61593
David Palmero <dapalm44@gmail.com>
2020-04-06 15:15:48 +0200

    Added spaces, check argv len

--- chall02/dapalmer.py
+++ chall02/dapalmer.py
@@ -3,42 +3,46 @@
 import sys
 
 def main():
-    morseCode = {
-        'A': '.-',
-        'B': '-...',
-        'C': '-.-.',
-        'D': '-..',
-        'E': '.',
-        'F': '..-.',   
-        'G': '--.',
-        'H': '....',
-        'I': '..',   
-        'J': '.---',
-        'K': '-.-',
-        'L': '.-..',   
-        'M': '--',
-        'N': '-.',
-        'O': '---',   
-        'P': '.--.',
-        'Q': '--.-',
-        'R': '.-.',
-        'S': '...',
-        'T': '-',
-        'U': '..-',
-        'V': '...-',
-        'W': '.--',
-        'X': '-..-',
-        'Y': '-.--',
-        'Z': '--..',       
-    }
-    origString = sys.argv[1].upper()
-    morseString = ''
+    if len(sys.argv) == 2:
+        morseCode = {
+            'A': '.-',
+            'B': '-...',
+            'C': '-.-.',
+            'D': '-..',
+            'E': '.',
+            'F': '..-.',   
+            'G': '--.',
+            'H': '....',
+            'I': '..',   
+            'J': '.---',
+            'K': '-.-',
+            'L': '.-..',   
+            'M': '--',
+            'N': '-.',
+            'O': '---',   
+            'P': '.--.',
+            'Q': '--.-',
+            'R': '.-.',
+            'S': '...',
+            'T': '-',
+            'U': '..-',
+            'V': '...-',
+            'W': '.--',
+            'X': '-..-',
+            'Y': '-.--',
+            'Z': '--..',
+            ' ': ' '       
+        }
+        origString = sys.argv[1].upper()
+        morseString = ''
 
-    for char in origString:
-        if char >= 'A' and char <= 'Z':
-            morseString += morseCode[char]
-
-    print morseString
+        for char in origString:
+            if char >= 'A' and char <= 'Z' or char == ' ':
+                morseString += morseCode[char]
+        print morseString
+    
+    else:
+        print 'Usage:', sys.argv[0], '<a-zA-Z>'
 
 if __name__ == "__main__":
     main()
