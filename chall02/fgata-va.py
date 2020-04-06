5aa793d9fa7b5dba9b0693f22552998c436142c0
Francisco Jesús Gata <fjgatv8998@gmail.com>
2020-04-06 13:31:41 +0200

    Quitting split

--- chall02/fgata-va.py
+++ chall02/fgata-va.py
@@ -9,67 +9,64 @@ if((len(sys.argv) > 2) or (len(sys.argv) == 1)):
     errorCode()
 if(len(sys.argv[1]) == 0):
     errorCode()
-
-arguments = sys.argv[1].split(' ')
 morse = ""
-
-for argu in arguments:
-    if((len(argu) == 0) or not(argu.isalpha())):
+for c in sys.argv[1].lower():
+    if(not(c.isalpha()) and not(c.isspace())):
         errorCode()
-    for c in argu.lower():
-        if (c == 'a'):
-            morse += '.-'
-        elif (c == 'b'):
-            morse += '-...'
-        elif (c == 'b'):
-            morse += '-...'
-        elif (c == 'c'):
-            morse += '-.-.'
-        elif (c == 'd'):
-            morse += '-..'
-        elif (c == 'e'):
-            morse += '.'
-        elif (c == 'f'):
-            morse += '..-.'
-        elif (c == 'g'):
-            morse += '--.'
-        elif (c == 'h'):
-            morse += '....'
-        elif (c == 'i'):
-            morse += '..'
-        elif (c == 'j'):
-            morse += '.---'
-        elif (c == 'k'):
-            morse += '.-.'
-        elif (c == 'l'):
-            morse += '.-..'
-        elif (c == 'm'):
-            morse += '--'
-        elif (c == 'n'):
-            morse += '-.'
-        elif (c == 'o'):
-            morse += '---'
-        elif (c == 'p'):
-            morse += '.--.'
-        elif (c == 'q'):
-            morse += '--.-'
-        elif (c == 'r'):
-            morse += '.-.'
-        elif (c == 's'):
-            morse += '...'
-        elif (c == 't'):
-            morse += '-'
-        elif (c == 'u'):
-            morse += '..-'
-        elif (c == 'v'):
-            morse += '...-'
-        elif (c == 'w'):
-            morse += '.--'
-        elif (c == 'x'):
-            morse += '-..-'
-        elif (c == 'y'):
-            morse += '-.--'
-        elif (c == 'z'):
-            morse += '--..'
-    morse += ' '
+    if (c == 'a'):
+        morse += '.-'
+    elif (c == 'b'):
+        morse += '-...'
+    elif (c == 'b'):
+        morse += '-...'
+    elif (c == 'c'):
+        morse += '-.-.'
+    elif (c == 'd'):
+        morse += '-..'
+    elif (c == 'e'):
+        morse += '.'
+    elif (c == 'f'):
+        morse += '..-.'
+    elif (c == 'g'):
+        morse += '--.'
+    elif (c == 'h'):
+        morse += '....'
+    elif (c == 'i'):
+        morse += '..'
+    elif (c == 'j'):
+        morse += '.---'
+    elif (c == 'k'):
+        morse += '.-.'
+    elif (c == 'l'):
+        morse += '.-..'
+    elif (c == 'm'):
+        morse += '--'
+    elif (c == 'n'):
+        morse += '-.'
+    elif (c == 'o'):
+        morse += '---'
+    elif (c == 'p'):
+        morse += '.--.'
+    elif (c == 'q'):
+        morse += '--.-'
+    elif (c == 'r'):
+        morse += '.-.'
+    elif (c == 's'):
+        morse += '...'
+    elif (c == 't'):
+        morse += '-'
+    elif (c == 'u'):
+        morse += '..-'
+    elif (c == 'v'):
+        morse += '...-'
+    elif (c == 'w'):
+        morse += '.--'
+    elif (c == 'x'):
+        morse += '-..-'
+    elif (c == 'y'):
+        morse += '-.--'
+    elif (c == 'z'):
+        morse += '--..'
+    elif (c == ' '):
+        morse += ' '
 print(morse)
