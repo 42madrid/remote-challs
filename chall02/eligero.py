802814732889990d26122229627f023b3e5c81df
EstebanLiG <esteban.elig@gmail.com>
2020-04-06 15:36:41 +0200

    Challenge 02: Constelación Serpens / String to Morse. As fast as possible

--- chall02/eligero.py
+++ chall02/eligero.py
@@ -6,19 +6,16 @@
 #*                                                    +:+ +:+         +:+     */
 #*   By: eligero <eligero@student.42.fr>            +#+  +:+       +#+        */
 #*                                                +#+#+#+#+#+   +#+           */
-#*   Created: 2020/04/02 15:01:31 by eligero           #+#    #+#             */
-#*   Updated: 2020/04/02 17:21:49 by eligero          ###   ########.fr       */
+#*   Created: 2020/04/06 14:01:31 by eligero           #+#    #+#             */
+#*   Updated: 2020/04/06 15:21:49 by eligero          ###   ########.fr       */
 #*                                                                            */
 #* ************************************************************************** */
 
 #
 # Constelación Serpens (Chall02) / String to Morse
 #
-# Solved with key-value pair dictionary. The achieved solution has the mindset
-# of continuous time problem, i.e manage and output each letter at time, without
-# memory allocation. In real morse each letter is transmited over radio
-# connection and it can be corrupted by radio noise. I understood that every
-# new command line argument means a new word, so I put a space between words.
+# Solved with key-value pair dictionary. The achieved solution has fits
+# the challenge problem's conceivement, by blacklisting wrongs behaviors.
 #
 
 import sys
@@ -31,23 +28,22 @@ MORSE_CODE_DICT = { 'A':'.-', 'B':'-...', 'C':'-.-.', 'D':'-..', 'E':'.',
 					'Z':'--..'}
 
 def encrypt(message):
+	cipher = ''
 	for char in message:
 		if (char >= 'a' and char <= 'z') or (char >= 'A' and char <= 'Z'):
-			print(MORSE_CODE_DICT[char.upper()], end = '')
+			cipher += MORSE_CODE_DICT[char.upper()]
 		elif char == ' ':
-			print(' ', end = '')
+			cipher += ' '
 		else:
-			print('#ERROR#', end = '')
+			cipher = 'usage: ./xlogin.py <a-zA-Z string>'
+			break
+	return cipher if cipher != '' else 'usage: ./xlogin.py <a-zA-Z string>'
 
 def main():
-	arguments = len(sys.argv) - 1
-	i = 1
-	while (arguments > i):
-		encrypt(sys.argv[i])
-		print(' ', end = '')
-		i += 1
-	encrypt(sys.argv[i])
-	print('')
+	if len(sys.argv) != 2:
+		print ('usage: ./xlogin.py <a-zA-Z string>')
+	else:
+		print (encrypt(sys.argv[1]))
 
 if __name__ == '__main__':
     main()
