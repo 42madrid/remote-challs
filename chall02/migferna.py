2bdf43c781f70f128715c4ed26a9989b7c17d11e
Miguel Ángel Fernández Carrillo <migferna@student.42madrid.com>
2020-04-06 12:31:40 +0200

    Firsh Version Chall02

---
+++ chall02/migferna.py
@@ -0,0 +1,45 @@
+import sys
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...', 
+                    'C':'-.-.', 'D':'-..', 'E':'.', 
+                    'F':'..-.', 'G':'--.', 'H':'....', 
+                    'I':'..', 'J':'.---', 'K':'-.-', 
+                    'L':'.-..', 'M':'--', 'N':'-.', 
+                    'O':'---', 'P':'.--.', 'Q':'--.-', 
+                    'R':'.-.', 'S':'...', 'T':'-', 
+                    'U':'..-', 'V':'...-', 'W':'.--', 
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..', 
+                    '1':'.----', '2':'..---', '3':'...--', 
+                    '4':'....-', '5':'.....', '6':'-....', 
+                    '7':'--...', '8':'---..', '9':'----.', 
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-', 
+                    '?':'..--..', '/':'-..-.', '-':'-....-', 
+                    '(':'-.--.', ')':'-.--.-'}
+
+def encrypt(message):
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
+
+            # Looks up the dictionary and adds the
+            # correspponding morse code
+            # along with a space to separate
+            # morse codes for different characters
+            cipher += MORSE_CODE_DICT[letter] + ''
+        else:
+            # 1 space indicates different characters
+            # and 2 indicates different words
+            cipher += ' '
+
+    return cipher
+
+def main(argv):
+  if ((len(argv) == 2) and (len(argv[1]) > 0)):
+    result = encrypt(argv[1].upper()) 
+    print (result) 
+  else:
+    print("usage: ./xlogin.py <a-zA-Z string>")
+
+# Executes the main function
+if __name__ == '__main__':
+    main(sys.argv)
