64eede33684753ed84b7362c0746fb0c33f98755
Eugeniu Stina <32365873+xplore93@users.noreply.github.com>
2020-04-07 16:06:15 +0200

    Update estina.py

--- chall02/estina.py
+++ chall02/estina.py
@@ -44,10 +44,10 @@ CHAR_TO_MORSE = {
 }
 
 if len(sys.argv) != 2 or not sys.argv[1]:
-	exit("usage " + sys.argv[0] + " <a-zA-Z string>")
+	exit("usage: " + sys.argv[0] + " <a-zA-Z string>")
 string = ""
 for c in sys.argv[1]:
 	if c.lower() not in CHAR_TO_MORSE:
-		exit("usage " + sys.argv[0] + " <a-zA-Z string>")
+		exit("usage: " + sys.argv[0] + " <a-zA-Z string>")
 	string += CHAR_TO_MORSE.get(c)
 print(string)
