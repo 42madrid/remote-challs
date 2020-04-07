819ad14d40780b96d527d41bd25afcc274e05abc
byrd92 <emilioggo@gmail.com>
2020-04-07 19:06:45 +0200

    pushed as executable python file

--- chall02/egarcia-.py
+++ chall02/egarcia-.py
@@ -1,15 +1,14 @@
+#!/usr/bin/env python3
+
 import sys
 
 morse = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..',
         'J': '.---','K': '-.-', 'L': '-.--', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
-        'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..'}
+        'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', ' ': ' '}
         
 if (len(sys.argv) == 2 and all(x.isalpha()  or  x.isspace() for x in sys.argv[1])):
     for letter in sys.argv[1]:
-        if letter == ' ':
-            print(' ',end="")
-        else :
             print(morse[letter.upper()],end="")
     print()
 else:
-    print('usage: ./xlogin.py <a-zA-Z string>')
+    print("usage: ./" + sys.argv[0] +  " <a-zA-Z string>")
