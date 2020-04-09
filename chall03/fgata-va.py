3a08a3c91bef152871097a46363a9330aab138b6
Francisco Jesús Gata <fjgatv8998@gmail.com>
2020-04-09 18:21:49 +0200

    Cleaning code

--- chall03/fgata-va.py
+++ chall03/fgata-va.py
@@ -2,7 +2,7 @@
 import sys
 
 def errorMessage(file_name):
-    print("usage: %s <1-9 squared_rows...>" % file_name)
+    print("usage: %s <1-9 matrixd_rows...>" % file_name)
     quit()
 
 def validateInput(args):
@@ -12,44 +12,48 @@ def validateInput(args):
         if(len(args[i]) != ylen or len(args[i]) != xlen or not(args[i].isnumeric())):
             errorMessage(args[0])
 
+def snailSorting(matrix, snailSort):
+    length = len(matrix)
+    maxlen = length * length
+    i = 0
+    while(i < maxlen):
+        # Go right
+        y = 0
+        for x in range(0, len(matrix[y])):
+            snailSort.append(matrix[y][x])
+            i += 1
+        del matrix[y]
+        y += 1
+        if(i >= maxlen):
+            break
+        # Go down
+        for y in range(0, len(matrix)):
+            snailSort.append(matrix[y][x])
+            del matrix[y][x]
+            i += 1
+        y = len(matrix) - 1
+        x -= 1
+        if(i >= maxlen):
+            break
+        # Go left
+        for x in range(x, -1, -1):
+            snailSort.append(matrix[y][x])
+            i += 1
+        del matrix[y]
+        if(i >= maxlen):
+            break
+        # Go up
+        for y in range(len(matrix)-1, -1, -1):
+            snailSort.append(matrix[y][x])
+            del matrix[y][x]
+            i += 1
+
 if (len(sys.argv) == 1):
     errorMessage(sys.argv[0])
 validateInput(sys.argv)
 snailSort = []
-square = sys.argv[1:]
-length = len(square)
-if(length == 1):
-    print(square[0])
-    quit()
-maxlen = length * length
-for i in range(0, len(square)):
-    square[i] = [int(j) for j in square[i]]
-i = 0
-while(i < maxlen):
-    y = 0
-    for x in range(0, len(square[y])):
-        snailSort.append(square[y][x])
-        i += 1
-    del square[y]
-    y += 1
-    if(i >= maxlen):
-        break
-    for y in range(0, len(square)):
-        snailSort.append(square[y][x])
-        del square[y][x]
-        i += 1
-    y = len(square) - 1
-    x -= 1
-    if(i >= maxlen):
-        break
-    for x in range(x, -1, -1):
-        snailSort.append(square[y][x])
-        i += 1
-    del square[y]
-    if(i >= maxlen):
-        break
-    for y in range(len(square)-1, -1, -1):
-        snailSort.append(square[y][x])
-        del square[y][x]
-        i += 1
+matrix = sys.argv[1:]
+for i in range(0, len(matrix)):
+        matrix[i] = [int(j) for j in matrix[i]]
+snailSorting(matrix, snailSort)
 print(*snailSort, sep=', ')
