8764969d594c6987d369b4cd4b8a8a6e51b58f5f
esepunto <esepunto@gmail.com>
2020-04-16 20:14:55 +0200

    Delete variable 'result': not necessary.

--- chall05/ssacrist.c
+++ chall05/ssacrist.c
@@ -2,24 +2,22 @@
 
 int     ft_ie_except_after_c(char* str)
 {
-    int     result;
     int     i;
 
     i = 0;
-    result = 1;
     while (str[i])
     {
         if (tolower(str[i]) == 'e')
         {
             if (tolower(str[i + 1]) == 'i' && (tolower(str[i - 1]) != 'c' || !str[i - 1]))
-                result = 0;
+                return(0);
         }
         if (tolower(str[i]) == 'i')
         {
             if (tolower(str[i + 1]) == 'e' && tolower(str[i - 1]) == 'c')
-                result = 0;
+                return(0);
         }
         i++;
     }
-    return(result);
+    return(1);
 }
