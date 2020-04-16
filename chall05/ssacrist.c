172f221d45aae6371ca1834894d303a3a86e2f59
esepunto <esepunto@gmail.com>
2020-04-16 18:34:46 +0200

    work with upper and lower case

--- chall05/ssacrist.c
+++ chall05/ssacrist.c
@@ -1,28 +1,26 @@
-#include <string.h>
+#include<ctype.h>
 
-int ft_ie_except_after_c(char* str)
+int     ft_ie_except_after_c(char* str)
 {
-    int  len;
-    int  result;
-    int  i;
-    
-    len = strlen(str);
+    int     result;
+    int     i;
+
     i = 0;
     result = 1;
-    while (i < len)
+    while (str[i])
     {
-        while (str[i] == 'e')
+        while (tolower(str[i]) == 'e')
         {
-            while (str[i + 1] == 'i' && (str[i - 1] != 'c' || !str[i - 1]))
+            while (tolower(str[i + 1]) == 'i' && (tolower(str[i - 1]) != 'c' || !str[i - 1]))
             {    
                 result = 0;
                 i++;
             }
             i++;
         }
-        while (str[i] == 'i')
+        while (tolower(str[i]) == 'i')
         {
-            while (str[i + 1] == 'e' && str[i - 1] == 'c')
+            while (tolower(str[i + 1]) == 'e' && tolower(str[i - 1]) == 'c')
             {    
                 result = 0;
                 i++;
