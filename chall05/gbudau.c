924ff477c717ec8d4649887be0f46a9894900a60
username <email@example.com>
2020-04-16 23:10:11 +0200

    chall05 - gbudau

---
+++ chall05/gbudau.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <ctype.h>
+
+int		ft_ie_except_after_c(char* str)
+{
+	char *start;
+
+	start = str;
+	while (*str)
+	{
+		if (tolower(*str) == 'c')
+			if (*(str + 1) && tolower(*(str + 1)) == 'i')
+				return (0);
+		if (tolower(*str) == 'e')
+		{
+			if (str - start > 0 && tolower(*(str - 1)) == 'c')
+			{
+				str++;
+				continue;
+			}
+			if (*(str + 1) && tolower(*(str + 1)) == 'i')
+				return (0);
+		}
+		str++;
+	}
+	return (1);
+}
