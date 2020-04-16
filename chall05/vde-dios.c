220564805be1ac3f84b77b4a42e9fe90d75e80be
Victor <victor.dedios@outlook.com>
2020-04-16 13:32:35 +0200

    update solution 2

--- chall05/vde-dios.c
+++ chall05/vde-dios.c
@@ -1,15 +1,20 @@
 int ft_ie_except_after_c(char* str)
 {
+    char *p;
+
     if (str)
     {
+        p = str;
         while (*str)
         {
-            if (*str == 'e' && *(str + 1))
+            if (*str == 'e')
             {
-                if (*(str - 1) == 'i' && *(str - 2) == 'c')
-                    return (0);
-                if (*(str + 1) == 'i' && *(str - 1) != 'c')
-                    return (0);
+                if (str != p && (str - 1)!= p)
+                    if (*(str - 1) == 'i' && *(str - 2) == 'c')
+                        return (0);
+                if (*(str + 1))
+                    if (*(str + 1) == 'i' && (str == p || *(str - 1) != 'c'))
+                        return (0);
             }
             str++;
         }
