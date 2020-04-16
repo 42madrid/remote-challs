24fc7343ab3af5153bef3309565cf5a8e2ab5d1b
Francisco Jesús Gata <fjgatv8998@gmail.com>
2020-04-16 13:39:01 +0200

    Small change

--- chall05/fgata-va.c
+++ chall05/fgata-va.c
@@ -1,18 +1,20 @@
 int		ft_ie_except_after_c(char *str)
 {
 	int i;
+	int follow_rule;
 
+	follow_rule = 1;
 	i = 0;
 	while (str[i] != '\0')
 	{
 		if ((str[i] == 'e' || str[i] == 'i'))
 		{
 			if (str[i] == 'i' && str[i + 1] == 'e' && (i > 0 && str[i - 1] == 'c'))
-				return (0);
+				follow_rule = 0;
 			else if (str[i] == 'e' && str[i + 1] == 'i' && (i == 0 || str[i - 1] != 'c'))
-				return (0);
+				follow_rule = 0;
 		}
 		i++;
 	}
-	return (1);
+	return (follow_rule);
 }
