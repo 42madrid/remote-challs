8e9a7d4f519dce557ebed3fdc433c5a020475a02
Victor <victor.dedios@outlook.com>
2020-04-16 14:23:26 +0200

    add ft_lower to ensure functionality with capital letters

--- chall05/vde-dios.c
+++ chall05/vde-dios.c
@@ -1,3 +1,10 @@
+char   ft_lower(char letter)
+{
+    if (letter >= 'A' && letter <= 'Z')
+        return (letter + 32);
+    return (letter);
+}
+
 int ft_ie_except_after_c(char* str)
 {
     char *p;
@@ -10,10 +17,10 @@ int ft_ie_except_after_c(char* str)
             if (*str == 'e')
             {
                 if (str != p && (str - 1)!= p)
-                    if (*(str - 1) == 'i' && *(str - 2) == 'c')
+                    if (ft_lower(*(str - 1)) == 'i' && ft_lower(*(str - 2)) == 'c')
                         return (0);
                 if (*(str + 1))
-                    if (*(str + 1) == 'i' && (str == p || *(str - 1) != 'c'))
+                    if (ft_lower(*(str + 1)) == 'i' && (str == p || ft_lower(*(str - 1)) != 'c'))
                         return (0);
             }
             str++;
