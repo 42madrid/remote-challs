a4d52ef7c27c30382d88d8f3f249d009086edb31
esepunto <esepunto@gmail.com>
2020-04-16 20:49:29 +0200

    Delete variable 'int i': not necessary.

--- chall05/ssacrist.c
+++ chall05/ssacrist.c
@@ -2,22 +2,17 @@
 
 int     ft_ie_except_after_c(char* str)
 {
-    int     i;
-
-    i = 0;
-    while (str[i])
+    while (*str)
     {
-        if (tolower(str[i]) == 'e')
-        {
-            if (tolower(str[i + 1]) == 'i' && (tolower(str[i - 1]) != 'c' || !str[i - 1]))
-                return(0);
-        }
-        if (tolower(str[i]) == 'i')
-        {
-            if (tolower(str[i + 1]) == 'e' && tolower(str[i - 1]) == 'c')
-                return(0);
-        }
-        i++;
+        if (tolower(*str) == 'e')
+            if (tolower(*(str + 1)) == 'i')
+                if (tolower(*(str - 1)) != 'c' || !*(str - 1))
+                    return(0);
+        if (tolower(*str) == 'i')
+            if (tolower(*(str + 1)) == 'e')
+                if (tolower(*(str - 1)) == 'c')
+                    return(0);
+        str++;
     }
     return(1);
 }
