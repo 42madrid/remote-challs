c191962664ffc67cb3d7cc588589e4abed175626
esepunto <esepunto@gmail.com>
2020-04-16 19:12:23 +0200

    Replace 'while' for 'if' for more efficient behavior

--- chall05/ssacrist.c
+++ chall05/ssacrist.c
@@ -9,23 +9,15 @@ int     ft_ie_except_after_c(char* str)
     result = 1;
     while (str[i])
     {
-        while (tolower(str[i]) == 'e')
+        if (tolower(str[i]) == 'e')
         {
-            while (tolower(str[i + 1]) == 'i' && (tolower(str[i - 1]) != 'c' || !str[i - 1]))
-            {    
+            if (tolower(str[i + 1]) == 'i' && (tolower(str[i - 1]) != 'c' || !str[i - 1]))
                 result = 0;
-                i++;
-            }
-            i++;
         }
-        while (tolower(str[i]) == 'i')
+        if (tolower(str[i]) == 'i')
         {
-            while (tolower(str[i + 1]) == 'e' && tolower(str[i - 1]) == 'c')
-            {    
+            if (tolower(str[i + 1]) == 'e' && tolower(str[i - 1]) == 'c')
                 result = 0;
-                i++;
-            }
-            i++;
         }
         i++;
     }
