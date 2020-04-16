b07f89f5b8353b7ac3e78bb4de87dabc214c0167
Victor <victor.dedios@outlook.com>
2020-04-16 12:36:15 +0200

    fetch upstream and add chall05 solution

---
+++ chall05/vde-dios.c
@@ -0,0 +1,19 @@
+int ft_ie_except_after_c(char* str)
+{
+    if (str)
+    {
+        while (*str)
+        {
+            if (*str == 'e' && *(str + 1))
+            {
+                if (*(str - 1) == 'i' && *(str + 1) == 'c')
+                    return (0);
+                if (*(str + 1) == 'i' && *(str + 2) != 'c')
+                    return (0);
+            }
+            str++;
+        }
+        return (1);
+    }
+    return (0);
+}
