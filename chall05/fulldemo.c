e974118724e4bd1a32650c2e0de7f6b86da2e7a9
fulldemo <fulldemo@MacBook-Pro-de-Ferran.local>
2020-04-16 13:17:07 +0200

    chall05

---
+++ chall05/fulldemo.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+int		ft_ie_except_after_c(char* str)
+{
+	int i;
+	int rule;
+
+	i = 0;
+	rule = 1;
+	while (str[i] != '\0')
+	{
+		if (str[i] == 'i')
+		{
+			if (str[i + 1] == 'e')
+			{
+				if (str[i - 1] == 'c')
+				{
+					rule = 0;
+					break;
+				}
+			}	
+			else if (str[i - 1] == 'e')
+			{
+				if (str[i - 2] != 'c')
+				{
+					rule = 0;
+					break;
+				}
+			}
+		}
+		i++;
+	}
+	return (rule);
+}
