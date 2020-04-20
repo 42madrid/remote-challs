6d4cc0998bf388c71608d9db48477d8d92bc32e4
Victor <victor.dedios@outlook.com>
2020-04-20 20:25:37 +0200

    fix colission errors

--- chall06/vde-dios.c
+++ chall06/vde-dios.c
@@ -1,18 +1,19 @@
 #include <math.h>
 #include <string.h>
-#include <stdio.h>
 #include <stdlib.h>
 
 typedef struct s_element{
     int x;
     int y;
+    int pos;
+    int side;
+    char id;
 }       t_element;
 
 int ft_check_format(char *board)
 {
-    char *set = ".KPBRQ\n";
     int i;
-    int size;
+    int side;
     int len;
 
     //Valid charset
@@ -20,18 +21,18 @@ int ft_check_format(char *board)
     len = strlen(board);
 
     while(++i < len)
-        if (!strchr(set, board[i]))
+        if (!strchr(".KPBRQ\n", board[i]))
             return (0);
     //Valid square
     if (len > 89)
         return (0);
-    size = sqrt(len);
-    i = size;
+    side = sqrt(len);
+    i = side;
     while (board[i] && i < len)
     {
         if (board[i] != '\n')
             return (0);
-        i += size + 1;
+        i += side + 1;
     }
     //Only one king
     if (strchr(strchr(board, 'K') + 1, 'K'))
@@ -39,77 +40,75 @@ int ft_check_format(char *board)
     return (1);
 }
 
-int ft_get_king_pos(char *board)
-{
-    int pos;
-
-    while (board[pos] != 'K')
-        pos++;
-    return (pos);
-}
-
-t_element ft_element_coord(int pos, int size)
+t_element ft_element_coord(int pos, int side, char *board)
 {
     t_element el;
 
-    el.x = pos / (size + 1);
-    el.y = pos - (size + 1) * el.x;
+    el.x = pos / (side + 1);
+    el.y = pos - (side + 1) * el.x;
+    el.pos = pos;
+    el.id = board[pos];
+    el.side = side;
     return (el);
 }
 
-int     ft_check_enemy(char *board, int pos, int size, t_element king, t_element enemy)
+int     ft_check_colission(t_element king, t_element enemy, char *board)
 {
-    char *hor = "RQ";
-    char *diag = "QPB";
-    if (strchr(hor, board[pos]))
+    int x;
+    int y;
+    
+    x = enemy.x - king.x;
+    y = enemy.y - king.y;
+    while (abs(x) > 1 ||abs(y) > 1)
     {
-         enemy = ft_element_coord(pos, size);
+        if (x)
+            x = x - (x > 0 ? 1 : -1);
+        if (y)
+            y = y - (y > 0 ? 1 : -1);
+        if (board[(king.side + 1) * (king.x + x) + (king.y + y)] != '.')
+            return (1);
+    }
+    return (0);
+}
+
+int     ft_check_enemy(t_element king, t_element enemy, char *board)
+{
+    if (strchr("RQ", enemy.id))
          if ((enemy.x == king.x ||enemy.y == king.y))
-            return (0);
-    }
-    if (strchr(diag, board[pos]))
-    {
-        enemy = ft_element_coord(pos, size);
+             return(!ft_check_colission(king, enemy, board));
+    if (strchr("QB", enemy.id))
         if (abs((enemy.x - king.x) / (enemy.y - king.y)) == 1)
-        {
-            if (board[pos] == 'P')
-            {
-                if (abs(enemy.x - king.x) == 1)
-                    return (0);
-            }
-            else
-                return (0);
-        }
-    }
-    return (1);
+             return(!ft_check_colission(king, enemy, board));
+    if (enemy.id == 'P')
+        if ((enemy.y - king.y) == 1 && abs((enemy.x - king.x)) == 1)
+             return(!ft_check_colission(king, enemy, board));
+    return (0);
 }
 
 int ft_check_mate(char *board)
 {
     int pos;
-    int size;
+    int side;
     t_element king;
     t_element enemy;
 
     if (!ft_check_format(board))
         return (1);
-    size = sqrt(strlen(board));
-    pos = ft_get_king_pos(board);
-    king = ft_element_coord(pos, size);
-    //Check forward
+    side = sqrt(strlen(board));
+    pos = 0;
+    while (board[pos] != 'K')
+        pos++;
+    king = ft_element_coord(pos, side, board);
+    pos = 0;
     while(board[pos])
     {
-        if(!ft_check_enemy(board, pos, size, king, enemy))
-            return (0);
+        if (strchr("RQPB", board[pos]))
+        {
+            enemy = ft_element_coord(pos, side, board);
+            if(ft_check_enemy(king, enemy, board))
+                return (0);
+        }
         pos++;
-    }
-    //Check backward
-    pos = ft_get_king_pos(board);
-    while(pos >= 0)
-    {        
-        if(!ft_check_enemy(board, pos, size, king, enemy))
-            return (0);
-        pos--;
     }
     return (1);
 }
