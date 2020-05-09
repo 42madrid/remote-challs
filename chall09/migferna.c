e981a18758e344c8648361b2ae32058a2c7ed730
Miguel Ángel Fernández Carrillo <mafernandez.code@gmail.com>
2020-05-09 21:40:17 +0200

    Valid/No Valid

--- chall09/migferna.c
+++ chall09/migferna.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 int parser(int seconds, int d)
 {
@@ -28,7 +30,7 @@ char *generate_output(int years, int months, int days, int hours, int minutes, i
 	years_count = years > 1 ? "years" : "year";
 	months_count = months > 1 ? "months" : "month";
 	days_count = days > 1 ? "days" : "day";
-	
+
 	sprintf(format, "%d %s, %d %s, %d %s, %d %s, %d %s, %d %s", years, years_count, months,months_count, days, days_count, hours, hours_count, minutes, minutes_count, seconds, seconds_count);
 	return (format);
 }
@@ -38,15 +40,17 @@ char *parser_format(char *s)
 	char *out;
 
 	int seconds = atoi(s);
-	int minutes;
-	int hours;
-	int days;
-	int months;
-	int years;
+	int minutes = 0;
+	int hours = 0;
+	int days = 0;
+	int months = 0;
+	int years = 0;
 
-	if (seconds == 0)
+	if (seconds == 0) {
 		out = "now";
-	else if (seconds > 60)
+		return out;
+	}
+	if (seconds > 60)
 	{
 		minutes = parser(seconds, 60);
 		seconds = seconds % 60;
@@ -78,20 +82,31 @@ char *parser_format(char *s)
 
 int	is_valid(char *seconds)
 {
-	while (*seconds)
+	char *ptr = strtok(strdup(seconds), " ");
+	int num_digits = 0;
+	while (ptr)
 	{
-		if (*seconds < '0' || *seconds > '9')
+		if (isdigit(*ptr))
+		{
+			num_digits++;
+			ptr = strtok(NULL, " ");
+		}
+		else if((*ptr == '+' || *ptr == '-') && !isdigit(*(ptr + 1)))
+			ptr = strtok(NULL, " ");
+		else
 			return 0;
-		seconds++;
 	}
+	if (num_digits > 1)
+		return 0;
 	return (1);
 }
 
 char *ft_format_duration(char *seconds)
 {
 	char *out;
-	if (is_valid(seconds) && *seconds)
-		out = parser_format(seconds);
+	if (*seconds && is_valid(seconds))
+		//out = parser_format(seconds);
+		out = "Valid";
 	else
 		out = "Invalid input.";
 	return out;
@@ -101,10 +116,18 @@ int main()
 {
 	printf("%s\n", ft_format_duration("62"));
 	printf("%s\n", ft_format_duration("3662"));
-	//printf("%s\n", ft_format_duration("0"));
-	printf("%s\n", ft_format_duration("-1"));
+	printf("%s\n", ft_format_duration("0"));
 	printf("%s\n", ft_format_duration("LOL42LOL"));
 	printf("%s\n", ft_format_duration(""));
 	printf("%s\n", ft_format_duration("2175984000"));
+	printf("%s\n", ft_format_duration("   42"));
+	printf("%s\n", ft_format_duration(" +  42"));
+	printf("%s\n", ft_format_duration("   42 1"));
+	printf("%s\n", ft_format_duration("42 1"));
+	printf("%s\n", ft_format_duration("42   "));
+	printf("%s\n", ft_format_duration("+42"));
+	printf("%s\n", ft_format_duration("-42"));
+	printf("%s\n", ft_format_duration("0042"));
+	printf("%s\n", ft_format_duration("0 42"));
 	return (0);
 }
