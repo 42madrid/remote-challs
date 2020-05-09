c158fe0177c12a7def155150f61f3883f58eeca7
Bruno Azuara <bruno.azuara@gmail.com>
2020-05-09 15:27:51 +0200

    internal division working

--- chall09/bazuara.c
+++ chall09/bazuara.c
@@ -6,7 +6,7 @@
 /*   By: bazuara <bazuara@student.42madrid.>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/09 14:07:16 by bazuara           #+#    #+#             */
-/*   Updated: 2020/05/09 15:18:59 by bazuara          ###   ########.fr       */
+/*   Updated: 2020/05/09 15:26:12 by bazuara          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,17 @@ char	*ft_format_duration(char *seconds)
 	hr = mn / 60;
 	dy = hr / 24;
 	yr = dy / 365;
-	printf("respre: s:%i m:%i, h:%i, d:%i,  y:%i\n", sc, mn, dy, hr, yr);
+	//printf("respre: s:%i m:%i, h:%i, d:%i,  y:%i\n", sc, mn, hr, dy, yr);
 	sc = sc % 60;
 	mn = mn % 60;
-	hr = hr % 60;
-	dy = dy % 24;
-	printf("result: s:%i m:%i, h:%i, d:%i,  y:%i\n", sc, mn, dy, hr, yr);
+	hr = hr % 24;
+	dy = dy % 365;
+	//printf("result: s:%i m:%i, h:%i, d:%i,  y:%i\n", sc, mn, hr, dy, yr);
 	return ("return");
 }
 
 int	main(void)
 {
-	ft_format_duration("3600");
+	//ft_format_duration("60");
+	ft_format_duration("31536000");
 }
