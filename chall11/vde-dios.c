ddb1958886ed873e987fb2052ee202884b9e8f76
Victor <victor.dedios@outlook.com>
2020-05-21 17:26:49 +0200

    fix 1 infinte loop

--- chall11/vde-dios.c
+++ chall11/vde-dios.c
@@ -8,7 +8,7 @@ char *ft_goto_parser(const char *code)
 {
     char *next;
     char *word;
-    char *statement = NULL;
+    char *aux = NULL;
     char *output = NULL;
     char *tmp = NULL;
     long n_line = 0;
@@ -17,33 +17,36 @@ char *ft_goto_parser(const char *code)
     next = strtok_r(input, "\n", &input);
     while (next)
     {
-        if (!strtol(next, &statement, 10) && errno == EINVAL)
+        if (!isdigit(*next))
         {
-            while(!isdigit(*statement))
-                statement++;
-            if (n_line >= atoi(statement))
-                return (strdup("Infinite loop !"));
-            else
+            aux = next;
+            while(!isdigit(*aux))
+                aux++;
+            next = strtok_r(input, "\n", &input);
+            while(!strstr(next, aux))
             {
                 next = strtok_r(input, "\n", &input);
-                while(!strstr(next, statement))
-                    next = strtok_r(input, "\n", &input);
+                if (!next)
+                    return (strdup("Infinite loop !"));
             }
         }
         else
         {
-            n_line = strtol(next, &statement, 10);
-            word = strtok_r(statement, " ", &statement);
+            while (isdigit(*next))
+                next++;
+            word = strtok_r(next, " ", &next);
             while(word)
             {
-                while(isspace(*word))
+                while(*word && isspace(*word))
                     word++;
-                asprintf(&output, "%s%s%s", tmp ? tmp: "", tmp ? " ": "", word);
-                free(tmp);
-                tmp = output;
-                word = strtok_r(statement, " ", &statement);
+                if (*word){
+                    asprintf(&output, "%s%s%s", tmp ? tmp: "", tmp ? " ": "", word);
+                    free(tmp);
+                    tmp = output;
+                }
+                word = strtok_r(next, " ", &next);
             }
-        next = strtok_r(input, "\n", &input);
+            next = strtok_r(input, "\n", &input);
         }
     }
     free(input);
